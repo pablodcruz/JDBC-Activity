f739a086470661964404a3d5e97a4d866b6bd0c5
pablodcruz <pablo.delacruz@revature.com>
2023-02-15 12:02:45 -0500

    Added Reflection API so stubs arent needed

--- src/main/java/Jdbc.java
+++ src/main/java/Jdbc.java
@@ -1,5 +1,4 @@
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -30,9 +29,9 @@ import util.ConnectionUtil;
  *      |3      |'Kashmir'          |'Led Zeppelin'         |
  *
  * Assignment: Write JDBC logic in the methods below to achieve the following in our database
- *  * NOTE: Open JDBCWalkthrough.md file for DQL and DML examples
- *      - write SQL logic to create a new song in our songs table
- *      - write SQL logic to retrieve all songs from songs table
+ *  * NOTE: Check JDBCWalkthrough.md file for DQL and DML examples
+ *      - write JDBC logic to create a new song in our songs table
+ *      - write JDBC logic to retrieve all songs from songs table
  *      - create a method that with the JDBC logic toretrieve a song from songs table
  *      - create a method that with the JDBC logic to delete a songs 
  *      - create a method that with the JDBC logic to update an entire songs 
@@ -43,16 +42,15 @@ public class Jdbc {
 
     //write JDBC logic to create a new song in our songs table
     public void createSong(Song song)  {
+        // 1. write JDBC code here
         try {
             //retrieve active connection to db
             Connection connection = ConnectionUtil.getConnection();
             //SQL statement we are going to execute, we use ? as placeholders we later set.
-            
-        // 1. write SQL statement here
             String sql = "change me";
-
             //create PrepareStatement object (better than Statement object)
             PreparedStatement pstmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
             //Add the song data to fill in the placeholders ? in the sql statement. 1 for the first ?, 2 for the second
             pstmt.setString(1, song.getTitle());
             pstmt.setString(2, song.getArtist());
@@ -64,17 +62,16 @@ public class Jdbc {
         }
     }
 
-    //write SQL logic to retrieve all songs from songs table
+    //write JDBC logic to retrieve all songs from songs table
     public List<Song> getAllSongs(){
         List<Song> songs = new ArrayList<>();
+        //2. write JDBC code here
         try{
         Connection connection = ConnectionUtil.getConnection();
-                
-    //2. write SQL statement here
         String sql = "change me";
-
         Statement statement = connection.createStatement();
         ResultSet rs = statement.executeQuery(sql);
+
         while(rs.next()){
                 songs.add(new Song(rs.getString(1), rs.getString(2)));
             }
@@ -85,12 +82,12 @@ public class Jdbc {
         return songs;
     }
 
-    //3. create a method  'getSong' with the JDBC logic to retrieve a song from songs table using the song id
+    //3. create a method that with the JDBC logic to retrieve a song from songs table using the song id
 
-    //4. create a method 'deleteSong' with the JDBC logic to delete a song, return the number of affected rows 
+    //4. create a method that with the JDBC logic to delete a songs, return the number of affected rows 
 
-    //5. create a method 'updateSong' with the JDBC logic to update an entire songs, return true if successful.  
+    //5. create a method that with the JDBC logic to update an entire songs, return true if successful.  
 
-    //6. create a method 'updateSongArtist' with the JDBC logic to update a songs artist 
+    //6. create a method that with the JDBC logic to update a songs artist 
 
 }
